dfa8b71c121e87c1f841a9e24b4b1f4a9ca550c1
Jorgeduque007 <ivanduque-2003@hotmail.com>
2025-01-19 22:54:52 -0500

    reser information

--- components/Information.tsx
+++ components/Information.tsx
@@ -5,12 +5,12 @@ import Image from "next/image";
 const Information: React.FC = () => {
   return (
     <>
-      <div className="flex lg:min-h-[calc(100vh-100px)] bg-gradient-to-b from-[#EEF5FF] to-[#F7FAFF]">
-        <div className=" grid grid-cols-1 lg:grid-cols-2 lg:items-center ">
+      <div className="flex items-center justify-center min-h-[calc(100vh-100px)] bg-gradient-to-b from-[#EEF5FF] to-[#F7FAFF]">
+        <div className="relative flex w-full max-w-[1200px] items-center gap-10">
           {/* Text Section */}
-          <div className="text-left p-10 md:w-3/4 lg:w-auto">
-            <h1 className="text-[55px] leading-[1.2] font-bold text-[#191847] mb-6 ">
-              Tu portal de información sobre los avances y resultados de la DSCI
+          <div className="flex-1">
+            <h1 className="text-[55px] leading-[1.2] font-bold text-[#191847] mb-6">
+              Tu portal de información sobre los avances y resultados de la DSCI..
             </h1>
             <a
               href="#"
@@ -20,29 +20,26 @@ const Information: React.FC = () => {
               <FaArrowRight className="ml-2" />
             </a>
           </div>
+
           {/* Image Section */}
-          <div className="relative px-20 lg:w-auto lg:px-0 flex justify-center items-center">
-        <div className="relative w-full ">
-          <div className="absolute inset-0 flex items-center justify-center">
-            <Image
-              src="/figure.png"
-              alt="Overlaid Shape"
-              className="w-full"
-              width={847}
-              height={677}
-            />
-          </div>
-          <div className="relative">
+          <div className="relative flex-1">
+            {/* People Image */}
             <Image
               src="/homeheader2.png"
               alt="People"
-              className="rounded-[20px] object-cover w-full"
+              className="rounded-[20px] object-cover"
               width={550}
               height={400}
             />
+            {/* Green Shape */}
+            <Image
+              src="/figure.png"
+              alt="Green Shape"
+              className="absolute top-0 right-0 transform translate-x-[50px] -translate-y-[50px] w-[200px] h-[200px] z-[-1]"
+              width={200}
+              height={200}
+            />
           </div>
-        </div>
-      </div>
         </div>
       </div>
     </>
