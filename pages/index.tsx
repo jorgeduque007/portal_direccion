5a121e7b214d6438cc9537bf745b82f04c56e242
Jorgeduque007 <ivanduque-2003@hotmail.com>
2025-01-22 10:25:40 -0500

    headers

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,8 +25,34 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Portal DSCI</title>
+        <title>
+          Portal | Dirección de Sustitución de Cultivos de Uso Ilícito
+        </title>
+        <meta
+          name="description"
+          content="El sitio web del Programa Nacional Integral de Sustitución de Cultivos de Uso Ilícito (PNIS) del gobierno de Colombia proporciona información detallada sobre los avances, proyectos y resultados del programa. Este portal busca promover la transparencia, la participación ciudadana y el desarrollo sostenible en las comunidades afectadas por cultivos ilícitos."
+        ></meta>
+        <meta
+          name="keywords"
+          content="PNIS, Programa Nacional Integral de Sustitución de Cultivos Ilícitos, Sustitución de cultivos, Avances PNIS, Gobierno de Colombia, Desarrollo sostenible, Participación ciudadana, Transparencia gubernamental, Cultivos ilícitos, Proyectos comunitarios"
+        ></meta>
+        <meta
+          name="author"
+          content="Dirección de Sustitucion de Cultivos de Uso Ilicito"
+        ></meta>
+        <meta
+          property="og:title"
+          content="Portal | Dirección de Sustitución de Cultivos de Uso Ilícito"
+        ></meta>
+        <meta
+          property="og:description"
+          content="El sitio web del Programa Nacional Integral de Sustitución de Cultivos de Uso Ilícito (PNIS) del gobierno de Colombia proporciona información detallada sobre los avances, proyectos y resultados del programa. Este portal busca promover la transparencia, la participación ciudadana y el desarrollo sostenible en las comunidades afectadas por cultivos ilícitos."
+        ></meta>
+        <meta property="og:type" content="website"></meta>
+        {/* <meta property="og:url" content="https://portal.direccionsustitucion-pnis.gov.co/">
+      <meta property="og:image" content="https://portal.direccionsustitucion-pnis.gov.co/images/DSC_30.png"></meta> */}
       </Head>
+
       <div>
         <Information />
         <Results />
