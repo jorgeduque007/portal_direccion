8a8544077e9012252bb76b8ee6cf3894a27f2ad2
Jorgeduque007 <ivanduque-2003@hotmail.com>
2025-02-10 12:30:29 -0500

    animation tests

--- components/Information.tsx
+++ components/Information.tsx
@@ -3,28 +3,45 @@ import { FaArrowRight } from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
 
+import { motion } from "framer-motion";
+
 const Information: React.FC = () => {
   return (
     <>
       <div className="flex lg:min-h-[calc(100vh-100px)] bg-gradient-to-b from-[#EEF5FF] to-[#F7FAFF]">
         <div className=" grid grid-cols-1 lg:grid-cols-2 lg:items-center ">
           {/* Text Section */}
-          <div className="text-left pl-20 lg:pl-56 py-10 lg:py-0  md:w-3/4 lg:w-auto">
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.5 }} // Se activa solo una vez cuando el 50% del elemento es visible
+            transition={{ duration: 0.8 }}
+            className="text-left pl-20 lg:pl-56 py-10 lg:py-0  md:w-3/4 lg:w-auto"
+          >
             <h1 className="text-[55px] font-sans leading-[1.2] font-bold text-[#191847] mb-6 ">
               Tu portal de información sobre los avances y resultados de la DSCI
             </h1>
-            <Link
-              href="/datos"
-              className="inline-flex items-center px-6 py-3 text-[19px] font-sans font-bold	 text-[#191847] bg-[#FFA000] rounded-full hover:bg-[#FF8C00] transition-all"
+            <motion.div
+              whileHover={{ scale: 1.1 }} // Aumenta de tamaño al hacer hover
+              whileTap={{ scale: 0.9 }} // Se reduce al hacer clic
             >
-              Ver datos y gráficos de avances
-              <FaArrowRight className="ml-2" />
-            </Link>
-          </div>
+              <Link
+                href="/datos"
+                className="inline-flex items-center px-6 py-3 text-[19px] font-sans font-bold	 text-[#191847] bg-[#FFA000] rounded-full hover:bg-[#FF8C00] transition-all"
+              >
+                Ver datos y gráficos de avances
+                <FaArrowRight className="ml-2" />
+              </Link>
+            </motion.div>
+          </motion.div>
           {/* Image Section */}
           <div className="relative px-20 lg:w-auto lg:px-0 flex justify-center items-center">
-            <div className="relative w-full ">
-              <div className="absolute inset-0 flex items-center justify-center">
+            <motion.div className="relative w-full ">
+              <motion.div
+                animate={{ rotate: 360 }}
+                transition={{ duration: 1 }}
+                className="absolute inset-0 flex items-center justify-center"
+              >
                 <Image
                   src="/figure.png"
                   alt="Overlaid Shape"
@@ -32,8 +49,14 @@ const Information: React.FC = () => {
                   width={847}
                   height={677}
                 />
-              </div>
-              <div className="relative">
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true, amount: 0.5 }} // Se activa solo una vez cuando el 50% del elemento es visible
+                transition={{ duration: 0.8 }}
+                className="relative"
+              >
                 <Image
                   src="/headerhd2.png"
                   alt="People"
@@ -41,8 +64,8 @@ const Information: React.FC = () => {
                   width={3015}
                   height={2675}
                 />
-              </div>
-            </div>
+              </motion.div>
+            </motion.div>
           </div>
         </div>
       </div>
--- components/Results.tsx
+++ components/Results.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 const Results: React.FC = () => {
   return (
     <div className="flex items-center lg:px-56 justify-center h-auto bg-white">
-      <div className="mx-6 lg:mx-0 md:w-full w-2/3 bg-gradient-to-b my-14 from-[#F7C95F] to-[#FDB235] rounded-[30px] p-10 flex flex-col justify-center">
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.5 }} // Se activa solo una vez cuando el 50% del elemento es visible
+        transition={{ duration: 0.8 }}
+        className="mx-6 lg:mx-0 md:w-full w-2/3 bg-gradient-to-b my-14 from-[#F7C95F] to-[#FDB235] rounded-[30px] p-10 flex flex-col justify-center"
+      >
         <h2 className="text-[43px] font-sans font-extrabold text-[#191847] mb-4 px-2-">
           Resultados en cifras.
         </h2>
@@ -22,10 +29,7 @@ const Results: React.FC = () => {
             <FaArrowRight className="ml-2" />
           </Link>
         </div>
-        
-      </div>
-
-
+      </motion.div>
     </div>
   );
 };
